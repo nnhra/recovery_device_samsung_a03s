1bd7ed62669ea71d05adda8ba9eadefe1ea7405b
Raka Prasetya Pratama <106611054+MizuNotCool@users.noreply.github.com>
2023-01-08 20:18:44 +0800

    a03s: only eng variant will be available

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,8 @@
 #
 # Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 ItzKaguya at ShirakawaProject
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_a03s-user
-add_lunch_combo twrp_a03s-userdebug
 add_lunch_combo twrp_a03s-eng
