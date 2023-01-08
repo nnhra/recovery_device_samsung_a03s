ea1d2cffa07a9b9914c34261d41e5ae9c655fbbe
Raka Prasetya Pratama <106611054+MizuNotCool@users.noreply.github.com>
2023-01-08 14:08:46 +0800

    a03s: rebrand omni to twrp

--- omni_a03s.mk
+++ omni_a03s.mk
@@ -1,25 +1,36 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2020 The Android Open Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Release name
+PRODUCT_RELEASE_NAME := a03s
 
-# Inherit from a03s device
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common TWRP stuff.
+# For PitchBlack use vendor/pb/config/common.mk
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
 $(call inherit-product, device/samsung/a03s/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
-# Device identifier. This must come after all inclusions
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_a03s
 PRODUCT_DEVICE := a03s
-PRODUCT_NAME := omni_a03s
+PRODUCT_MODEL := Galaxy A03s
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A037F
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
